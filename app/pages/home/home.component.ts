f5ae83aee396d238b721d7c757dc949eec12c61d
Panagiotis Lampropoulos <plambros@otenet.gr>
2018-05-18 16:16:17 +0300

    now using vocab's extra for category icon urls

--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -3,9 +3,10 @@
  */
 import {Component, OnInit} from "@angular/core";
 import {FormBuilder, FormGroup} from "@angular/forms";
-import {Service} from "../../domain/eic-model";
+import {Vocabulary} from "../../domain/eic-model";
 import {SearchQuery} from "../../domain/search-query";
 import {NavigationService} from "../../services/navigation.service";
+import {ResourceService} from "../../services/resource.service";
 
 @Component({
     selector: "home",
@@ -14,40 +15,23 @@ import {NavigationService} from "../../services/navigation.service";
 })
 export class HomeComponent implements OnInit {
     public searchForm: FormGroup;
-    public categories: category[] = [
-        {
-            value: "Authentication and authorization infrastructure",
-            icon: "fingerprint_scanning_authorization.svg",
-            hover: "fingerprint_scanning_authorization_hover.svg"
-        },
-        {value: "Compute", icon: "cloud_server.svg", hover: "cloud_server_hover.svg"},
-        {value: "Connectivity", icon: "connectivity.svg", hover: "connectivity_hover.svg"},
-        {value: "Consulting", icon: "consulting.svg", hover: "consulting_hover.svg"},
-        {value: "Operations", icon: "keyword.svg", hover: "keyword_hover.svg"},
-        {value: "Content delivery", icon: "data-connectivity.svg", hover: "data-connectivity_hover.svg"},
-        {value: "Data discovery", icon: "keyword.svg", hover: "keyword_hover.svg"},
-        {value: "Data movement", icon: "database.svg", hover: "database_hover.svg"},
-        {value: "Data registration", icon: "cloud_server.svg", hover: "cloud_server_hover.svg"},
-        {value: "Data storage", icon: "database_security.svg", hover: "database_security_hover.svg"}
-    ];
-    private services: Service[];
+    public categories: Vocabulary[];
+    public baseIconURI = "./imgs/Icons/";
 
-    constructor(public fb: FormBuilder, public router: NavigationService) {
+    constructor(public fb: FormBuilder, public router: NavigationService, public resourceService: ResourceService) {
         this.searchForm = fb.group({"query": [""]});
     }
 
     ngOnInit() {
-        //fetch categories, check size, skip unpopulated ones here
+        this.resourceService.getVocabulariesRaw("Category").subscribe(suc => {
+            this.categories = suc.results
+            .map(e => Object.assign(e, {extra: e.extra || ["no_icon.svg", "no_icon.svg"]}))
+            .filter(e => e.id !== "Category-Other" && e.extra && e.extra.length && e.extra.length === 2);
+        });
     }
 
     onSubmit(searchValue: SearchQuery) {
         return this.router.search({query: searchValue.query});
     }
-}
-
-class category {
-    value: string;
-    icon: string;
-    hover: string;
 }
 
