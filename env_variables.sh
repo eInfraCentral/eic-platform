8f6c8674cdad7650425c6aed023c7468fc7d32a3
Konstantinos Spyrou <spyroukon@gmail.com>
2019-03-29 13:19:24 +0200

    fix

--- env_variables.sh
+++ env_variables.sh
@@ -3,5 +3,5 @@
 envsubst '${MATOMO_HOST} ${MATOMO_SITE_ID}' < /usr/share/nginx/html/index.html.tmpl > /usr/share/nginx/html/index.html &&
 envsubst '${PLATFORM_API_ENDPOINT}' < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf &&
 
-nginx -s reload;
+nginx -g "daemon off;"
 
